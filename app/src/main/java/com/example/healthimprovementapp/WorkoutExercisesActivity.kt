7c10151fa6f8adbfb8f5b0960e6d5db74a2fa699
Owen Knott <oknott14@outlook.com>
2020-11-17 14:50:46 -0500

    added layout for WorkoutExercises.kt, added custom list view for layout, added footer view for list for submission.

--- app/src/main/java/com/example/healthimprovementapp/WorkoutExercisesActivity.kt
+++ app/src/main/java/com/example/healthimprovementapp/WorkoutExercisesActivity.kt
@@ -1,7 +1,29 @@
 package com.example.healthimprovementapp.com.example.healthimprovementapp
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.healthimprovementapp.R
 
 class WorkoutExercisesActivity : AppCompatActivity() {
+
+    private lateinit var mListView : ListView
+    private lateinit var mSubmitButton : Button
+
+ 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout_exercise)
+
+        //Set mListView and add mSubmitButton as a footer to the list
+        mListView = findViewById<ListView>(R.id.exerciseList)
+        mListView.setFooterDividersEnabled(true)
+        mSubmitButton = findViewById(R.id.submitButton)
+        mListView.addFooterView(mSubmitButton)
+
+
+    }
 
 }
